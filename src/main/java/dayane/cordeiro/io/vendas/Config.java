c18d96fc7b2ec340773b739ce60f7da373af20c3
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2024-01-30 20:11:13 -0300

    feat: running profiles on spring boot

--- src/main/java/dayane/cordeiro/io/vendas/Config.java
+++ src/main/java/dayane/cordeiro/io/vendas/Config.java
@@ -1,8 +1,18 @@
 package dayane.cordeiro.io.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod")
 public class Config {
+
+    @Bean
+    public CommandLineRunner exec() {
+        return args -> {
+            System.out.println("Running config");
+        };
+    }
 }
