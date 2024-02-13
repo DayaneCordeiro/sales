d4fb29d2d8e4e0f4f905d2c3ebb554f3238367c4
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2024-02-13 15:58:39 -0300

    feat: saving and getting clients

--- src/main/java/dayane/cordeiro/io/vendas/VendasApplication.java
+++ src/main/java/dayane/cordeiro/io/vendas/VendasApplication.java
@@ -1,12 +1,30 @@
 package dayane.cordeiro.io.vendas;
 
+import dayane.cordeiro.io.vendas.domain.entity.Client;
+import dayane.cordeiro.io.vendas.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+	@Bean
+	public CommandLineRunner init(@Autowired ClientRepository repository) {
+		return args -> {
+			repository.save(new Client("Tatazinha"));
+			repository.save(new Client("Day"));
+
+			List<Client> clients = repository.getAll();
+			clients.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 
