79a07757c1e3be7b3aa10e23b7005a7c62c4ad81
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2024-02-13 15:25:11 -0300

    feat: entities

---
+++ src/main/java/dayane/cordeiro/io/vendas/domain/entity/OrdersItems.java
@@ -0,0 +1,41 @@
+package dayane.cordeiro.io.vendas.domain.entity;
+
+public class OrdersItems {
+
+    private Integer id;
+    private Orders order;
+    private Product product;
+    private Integer amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Orders getOrder() {
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/dayane/cordeiro/io/vendas/domain/entity/Product.java
@@ -0,0 +1,34 @@
+package dayane.cordeiro.io.vendas.domain.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Integer id;
+    private String description;
+    private BigDecimal unityPrice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnityPrice() {
+        return unityPrice;
+    }
+
+    public void setUnityPrice(BigDecimal unityPrice) {
+        this.unityPrice = unityPrice;
+    }
+}
